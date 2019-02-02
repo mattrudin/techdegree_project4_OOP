5a8a16af05fb139909193c367debaad97787f0e2
Matthias Rudin <matthias.rudin@live.com>
2019-02-02 15:03:54 +0100

    Added Phrase structure with methods

--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,36 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * Phrase.js */
+/**
+ * Represents a Phrase.
+ * @constructor
+ * @param {string} phrase - Phrase for the game. Will be converted to lower case.
+ */
+
+class Phrase {
+    constructor(phrase) {
+        this._phrase = phrase.toLowerCase();
+    }
+
+    /**
+     * Adds the phrase to the HTML file
+     * @returns {void}
+     */
+    addPhraseToDisplay() {
+
+    }
+
+    /**
+     * Checks if the given letter is matched within the phrase
+     * @param {string} - Character
+     * @returns {boolean} - if the phrase contains the letter
+     */
+    checkLetter() {
+
+    }
+
+    /**
+     * Reveals the matched letters on the HTML file
+     * @returns {void}
+     */
+    showMatchedLetter() {
+        
+    }
+}
