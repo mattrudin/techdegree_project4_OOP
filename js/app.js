0bd95ed3cf412e457e1b7ce2acc729e516136f5b
Matthias Rudin <matthias.rudin@live.com>
2019-02-03 20:53:00 +0100

    Added eventListener

--- js/app.js
+++ js/app.js
@@ -6,12 +6,19 @@ const phrases = [
     `How are you`,
     `What do you do`,
     `Do you like javascript`,
-    `I don't like java`
+    `I dont like java`
 ]
 
+let game;
 
 const startButton = document.getElementById('btn__reset');
 startButton.addEventListener('click', () => {
-    const game = new Game(phrases);
+    game = new Game(phrases);
     game.startGame();
+})
+
+const keyboard = document.getElementById('qwerty');
+keyboard.addEventListener('click', event => {
+    const letter = event.target.textContent; 
+    if(letter.length === 1) game.handleInteraction(letter);
 })
